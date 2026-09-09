e409f52be2c9824e0ec179eec737f68d32a915de
safna <safnaaboobacker05@gmail.com>
2026-09-09 22:30:31 +0530

    committed..

--- day_01/01_number_check.py
+++ day_01/01_number_check.py
@@ -2,3 +2,19 @@
 write a program to check number is +ve -ve or zero
 
 """
+number = int(input("Enter the number:"))
+
+if number > 0:
+
+    print("Number is positive")
+
+elif number < 0:
+    print("Number is Negative")
+
+else:
+
+    print("Number is zero")
+
+
+
+
